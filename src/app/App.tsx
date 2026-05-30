33e47e824c611ceddd094e8b0cd4f98dc9aa8dfa
Alan Bryant <alan@puzzlefactory.io>
2026-05-30 14:08:24 -0400

    first change

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ export default function App() {
     <div className="size-full flex items-center justify-center">
       <div className="text-center">
         <h1 className="mb-4">Welcome to Figma Make</h1>
-        <p>This is a simple page with some text on it.</p>
+        <p>Locally grown, changed and pushed</p>
       </div>
     </div>
   );
